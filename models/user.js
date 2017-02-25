e18d6dc824f49281de11ebca5800df47250fe68f
Isaias <impomales@gmail.com>
2017-02-25 20:15:32 +0000

    add user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,21 @@
 var mongoose = require('mongoose');
 
 var user = new mongoose.Schema({
-    
+    displayName: String,
+    name: {
+        firstName: String,
+        middleName: String,
+        lastName: String
+    },
+    watchlists: {
+        type: [{type: mongoose.Schema.Types.ObjectId, ref: 'Watchlist'}],
+        validate: [max_array_length, '{PATH} exceeds array length of 3']
+    }
 });
+
+// at most 3 watchlists per user.
+function max_array_length(value) {
+    return value.length <= 3;
+}
 
 module.exports = mongoose.model('User', user);
