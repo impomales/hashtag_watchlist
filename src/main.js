c216de30beb529759bb6499eae6f756426e4d9b8
Isaias <impomales@gmail.com>
2017-02-27 04:57:59 +0000

    add css and image files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Redirect = require('react-router').Redirect;
+
+class NoMatch extends React.Component {
+    render() {
+        return (
+            <h2>ERROR: NO MATCH FOUND FOR THIS ROUTE</h2>
+        );
+    }
+}
 
 ReactDOM.render(
-    <h1>Hello, World!</h1>,
+    <NoMatch />,
     document.getElementById('root')
 );
